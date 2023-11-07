4b7592a4a3dc024a9d9291fd1cdd55b6414a700f
yadavanuj44 <95302917+yadavanuj44@users.noreply.github.com>
2023-11-07 16:55:32 +0530

    added header and nav

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import Header from "@/components/header";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -18,10 +19,11 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${inter.className} bg-zinc-50 text-zinc-950 relative w-[100dvw] h-[100dvh]`}
+        className={`${inter.className} bg-zinc-50 text-zinc-950 relative h-[5000px]`}
       >
-        <div className="bg-[#fbe2e3] absolute -z-10 rounded-full blur-[10rem] w-[50%] h-[50%] top-0 left-0"></div>
-        <div className="bg-[#dbd7fb] absolute -z-10 rounded-full blur-[10rem] w-[50%] h-[50%] top-0 right-0"></div>
+        <div className="bg-[#fbe2e3] absolute -z-10 rounded-full blur-[10rem] w-[50%] h-[32rem] top-0 left-0"></div>
+        <div className="bg-[#dbd7fb] absolute -z-10 rounded-full blur-[10rem] w-[50%] h-[32rem] top-0 right-0"></div>
+        <Header />
         {children}
       </body>
     </html>
