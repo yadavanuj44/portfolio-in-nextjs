12474d504a5c902d77b0431790843f2aec8d87b7
yadavanuj44 <95302917+yadavanuj44@users.noreply.github.com>
2023-11-10 10:58:27 +0530

    Update footer.tsx

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,9 +1,15 @@
 export default function Footer() {
   return (
     <footer className="mb-10 px-4 text-center text-slate-500">
-      <small className="mb-2 block text-xs">
+      <small className="block text-xs">
         &copy; 2023 Anuj Yadav. All rights reserved.
       </small>
+      <a
+        className="text-xs underline" target="_blank"
+        href="https://github.com/yadavanuj44/portfolio-in-nextjs"
+      >
+        View Code
+      </a>
       <p className="text-xs">
         Thid website is built with React & Next.js 14 (App Router & Server
         Actions), TypeScript, Tailwind CSS, Framer Motion & Resend.
